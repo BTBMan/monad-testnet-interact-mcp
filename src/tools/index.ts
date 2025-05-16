53f7c3814e3d227e2d9c5c0e6cccb83523e795aa
BTBMan <1498118157@qq.com>
2025-05-16 17:51:09 +0800

    chore: rename tool files
    Signed-off-by: BTBMan <1498118157@qq.com>

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,7 +1,7 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-import { getBalance } from './getBalance.js';
-import { getBlock } from './getBlock.js';
-import { getTransaction } from './transaction.js';
+import { getBalance } from './get-balance.js';
+import { getBlock } from './get-block.js';
+import { getTransaction } from './get-transaction.js';
 
 export const registerTools = (server: McpServer) => {
   getBalance(server);
